46c57c8ab2e685a65bdd71e065b60370c4f32619
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-06-10 18:07:36 -0500

    Ran first test now all work under 10, I made it where if its under 5 dollars don't apply a dollar discount. technically you could by something for 5.01 and just pay shipping.

--- coupon_calculations.py
+++ coupon_calculations.py
@@ -1,5 +1,19 @@
+SHIPPING_UNDER_10 = 5.95
+SHIPPING_BETWEEN_30_10 = 7.95
+SHIPPING_BETWEEN_30_50 = 11.95
+TAX = .06
+
+
 def calculate_order(price, cash_coupon, percent_coupon):
-    pass
+    if 10 > price > cash_coupon:
+        total = price - cash_coupon
+        total = total - (total * percent_coupon)
+        total = (total*(1 + TAX) + SHIPPING_UNDER_10)
+        return round(total, 2)
+    else:
+        total = price - (price * percent_coupon)
+        total = (total*(1 + TAX) + SHIPPING_UNDER_10)
+        return round(total, 2)
 
 
 if __name__ == '__main__':
--- test/test_coupon_calculations.py
+++ test/test_coupon_calculations.py
@@ -5,22 +5,22 @@ from TOP3.coupon_calculations import calculate_order
 
 class MyTestCase(unittest.TestCase):
     def test_coupon_calculation_under_10(self):
-        self.assertEqual(calculate_order(4, 5, .10), False)
+        self.assertEqual(calculate_order(4, 5, .10), 9.77)
 
     def test_coupon_calculation_under_10(self):
-        self.assertEqual(calculate_order(7.50, 5, .15), False)
+        self.assertEqual(calculate_order(7.50, 5, .15), 8.20)
 
     def test_coupon_calculation_under_10(self):
-        self.assertEqual(calculate_order(9, 5, .20), False)
+        self.assertEqual(calculate_order(9, 5, .20), 9.34)
 
     def test_coupon_calculation_under_10(self):
-        self.assertEqual(calculate_order(6.55, 10, .10), False)
+        self.assertEqual(calculate_order(6.55, 10, .10), 12.20)
 
     def test_coupon_calculation_under_10(self):
-        self.assertEqual(calculate_order(3.95, 10, .15), False)
+        self.assertEqual(calculate_order(3.95, 10, .15), 9.51)
 
     def test_coupon_calculation_under_10(self):
-        self.assertEqual(calculate_order(8.92, 10, .20), False)
+        self.assertEqual(calculate_order(8.92, 10, .20), 13.51)
 
 
 if __name__ == '__main__':
