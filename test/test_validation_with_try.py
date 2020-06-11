8b34e9598dcd703eebbd5e36a25a3a0ed5ed0919
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-06-10 21:51:45 -0500

    Third score being negative, test will fail.

--- test/test_validation_with_try.py
+++ test/test_validation_with_try.py
@@ -7,10 +7,14 @@ class MyTestCase(unittest.TestCase):
     def test_average_exception1(self):
         with self.assertRaises(ValueError):
             average(-90, 89, 78)
+
     def test_average_exception2(self):
         with self.assertRaises(ValueError):
             average(90, -89, 78)
 
+    def test_average_exception3(self):
+        with self.assertRaises(ValueError):
+            average(90, 89, -78)
 
 
 if __name__ == '__main__':
