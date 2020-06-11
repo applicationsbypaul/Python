4fae8ce23ffeaa4212b4f0964fcbfd6fb0d786c6
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-06-10 21:35:18 -0500

    raised the ValueError so test will pass.

--- average_scores.py
+++ average_scores.py
@@ -9,6 +9,8 @@ display average score with information
 
 def average(score1, score2, score3):
     NUMBER_TESTS = 3
+    if score1 < 0:
+        raise ValueError
     return float((score1 + score2 + score3) / NUMBER_TESTS)
 
 
