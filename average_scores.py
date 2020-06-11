201de0e87f9e0f4c46d2a8daac881f1459cf696f
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-06-10 21:50:24 -0500

    Second number being negative, fixed code now test pass

--- average_scores.py
+++ average_scores.py
@@ -11,6 +11,8 @@ def average(score1, score2, score3):
     NUMBER_TESTS = 3
     if score1 < 0:
         raise ValueError
+    elif score2 < 0:
+        raise ValueError
     return float((score1 + score2 + score3) / NUMBER_TESTS)
 
 
--- test/test_validation_with_try.py
+++ test/test_validation_with_try.py
@@ -4,9 +4,13 @@ from average_scores import average
 
 
 class MyTestCase(unittest.TestCase):
-    def test_average_exception(self):
+    def test_average_exception1(self):
+        with self.assertRaises(ValueError):
+            average(-90, 89, 78)
+    def test_average_exception2(self):
         with self.assertRaises(ValueError):
             average(90, -89, 78)
+
 
 
 if __name__ == '__main__':
