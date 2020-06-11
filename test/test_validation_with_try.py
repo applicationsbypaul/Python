ee97556719f7a1aca3457a92fdd2a08a5fda5a30
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-06-10 21:44:06 -0500

    Second number being negative fails the test.

--- test/test_validation_with_try.py
+++ test/test_validation_with_try.py
@@ -6,7 +6,7 @@ from average_scores import average
 class MyTestCase(unittest.TestCase):
     def test_average_exception(self):
         with self.assertRaises(ValueError):
-            average(-90, 89, 78)
+            average(90, -89, 78)
 
 
 if __name__ == '__main__':
